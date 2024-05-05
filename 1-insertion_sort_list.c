24db6ac732746f1de0c68792175749c123eb6348
Moses-Aduojo <temojokego@gmail.com>
2024-05-04 18:12:47 -0700

    seg fault handled

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,11 +7,13 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = (*list)->next;
+	listint_t *current = NULL;
 	listint_t *temp, *tracker = NULL;
 
 	if (list == NULL || *list == NULL)
-		exit(EXIT_FAILURE);
+		return;
+
+	current = (*list)->next;
 	while (current != NULL)
 	{
 		tracker = current;
