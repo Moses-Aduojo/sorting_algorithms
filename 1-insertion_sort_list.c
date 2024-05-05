516f0a09f3397cafc2b4b33df702fabd0504add1
Moses-Aduojo <temojokego@gmail.com>
2024-05-04 18:01:38 -0700

    editted

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *temp, *tracker = NULL;
 
 	if (list == NULL || *list == NULL)
-		return;
+		exit(EXIT_FAILURE);
 	while (current != NULL)
 	{
 		tracker = current;
